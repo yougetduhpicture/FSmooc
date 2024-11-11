e26cb1dba7374af713c1370b935e2d4daa6f085e
Yougetduhpicture <ruonakoski.rasmus@gmail.com>
2024-11-11 13:53:17 +0200

    feat: 4.21 update

--- 3/models/person.js
+++ 3/models/person.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const url = process.env.MONGODB_URI.toString();
+const url = process.env.MONGODB_URI;
 
 console.log("connecting to", url);
 mongoose
-  .connect(url)
+  .connect(url.toString())
   .then((result) => {
     console.log("connected to MongoDB");
   })
