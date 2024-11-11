a35fcfbf111e52234bd8ca0bd1df05eb459e0166
Yougetduhpicture <ruonakoski.rasmus@gmail.com>
2024-11-11 21:22:29 +0200

    feat: 3.21 done

--- 3/index.js
+++ 3/index.js
@@ -1,29 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
 
-const app = express();
-const cors = require("cors");
-const path = require("path");
-const Person = require("./models/person");
+const app = express()
+const cors = require('cors')
+const path = require('path')
+const Person = require('./models/person')
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(express.static(path.join(__dirname, "build")));
-app.use(express.json());
-app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.json())
+app.use(cors())
 
-morgan.token("body", function (req, res) {
-  return JSON.stringify(req.body);
-});
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
 
 app.use(
   morgan(function (tokens, req, res) {
@@ -31,127 +31,127 @@ app.use(
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       tokens.body(req, res),
-    ].join(" ");
+    ].join(' ')
   })
-);
+)
 
 // GET FRONT END
-app.get("/", (req, res, next) => {
+app.get('/', (req, res, next) => {
   try {
-    res.sendFile(path.join(__dirname, "/build", "index.html"));
+    res.sendFile(path.join(__dirname, '/build', 'index.html'))
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-// GET INFO
-app.get("/info", (req, res, next) => {
+//GET INFO
+app.get('/info', (req, res, next) => {
   try {
-    const date = new Date();
-    const peopleCount = Person.length; // Assuming Person is an array of people
+    const date = new Date()
+    const peopleCount = Person.length // Assuming Person is an array of people
 
     res.send(
       `<p>Phonebook has info for ${peopleCount} people</p><br><p>${date}</p>`
-    );
+    )
   } catch (error) {
-    next(error); // Pass the error to Express error handler
+    next(error) // Pass the error to Express error handler
   }
-});
+})
 
-// GET ALL
-app.get("/api/people", (request, response, next) => {
-  console.log("Getting people");
+//GET ALL
+app.get('/api/people', (request, response, next) => {
+  console.log('Getting people')
   Person.find({})
     .then((people) => {
       if (people) {
-        response.json(people);
+        response.json(people)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // GET WITH ID
-app.get("/api/people/:id", (request, response, next) => {
+app.get('/api/people/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // DELETE
-app.delete("/api/people/:id", (request, response, next) => {
+app.delete('/api/people/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const generateId = () => {
   function getRandomInt(min, max) {
-    min = Math.ceil(1000000);
-    max = Math.floor(9999999);
-    return Math.floor(Math.random() * (max - min) + min);
+    min = Math.ceil(1000000)
+    max = Math.floor(9999999)
+    return Math.floor(Math.random() * (max - min) + min)
   }
-  return getRandomInt();
-};
+  return getRandomInt()
+}
 
 // ADD PERSON
-app.post("/api/people", (request, response, next) => {
-  const body = request.body;
-  console.log(body.content);
+app.post('/api/people', (request, response, next) => {
+  const body = request.body
+  console.log(body.content)
   if (!body.name || !body.number) {
-    return response.status(400).json({ error: "content missing" });
+    return response.status(400).json({ error: 'content missing' })
   }
 
   const person = new Person({
     id: generateId(),
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      console.log("saving a person");
-      response.json(savedPerson);
+      console.log('saving a person')
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-// UPDATE PERSON
-app.put("/api/people/:id", (request, response, next) => {
-  const body = request.body;
+//UPDATE PERSON
+app.put('/api/people/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     id: body.id,
     content: body.name,
     number: body.number,
-  };
-  console.log(person);
+  }
+  console.log(person)
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- 3/models/person.js
+++ 3/models/person.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const phoneBookSchema = new mongoose.Schema({
   id: String,
@@ -25,7 +25,7 @@ const phoneBookSchema = new mongoose.Schema({
     minlength: 8,
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{5,}/.test(v);
+        return /\d{2,3}-\d{5,}/.test(v)
       },
       message: (
         props
@@ -33,14 +33,14 @@ const phoneBookSchema = new mongoose.Schema({
       The correct format is 2-3 numbers then - then 5+ numbers`,
     },
   },
-});
+})
 
-phoneBookSchema.set("toJSON", {
+phoneBookSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", phoneBookSchema);
+module.exports = mongoose.model('Person', phoneBookSchema)
