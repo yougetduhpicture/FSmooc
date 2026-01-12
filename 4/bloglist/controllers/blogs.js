ca3e6ad07596be49729b3d4a0f26c9fa1a87463d
Rasmus Ruonakoski <ruonakoski.rasmus@gmail.com>
2026-01-12 14:01:46 +0200

    feat: 4.9. done
    Made the test and changed the router.

--- 4/bloglist/controllers/blogs.js
+++ 4/bloglist/controllers/blogs.js
@@ -1,17 +1,19 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+})
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
